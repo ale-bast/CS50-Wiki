c1aceac35d30eeb6b35be026fcb7ec0ff70bbe40
ale-bast <axellebastard@gmail.com>
2023-07-14 21:06:58 +0200

    Initial commit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
     path("create/", views.create, name="create"),
     path("edit/<str:title>", views.edit, name="edit"),
